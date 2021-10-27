82ebddfedb2e822ce537d9dd1af226df79fc3bc0
Carlos Pereira <carlos.l.pereira@gmail.com>
2021-10-27 22:53:01 +0200

    Fix missing arguments to installer options

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ download_dir=$default_download_dir
 
 # Install Google Cloud SDK
 pushd $download_dir
+mkdir -p $HOME/.local/opt
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-362.0.0-linux-x86_64.tar.gz
 tar xzf google-cloud-sdk-362.0.0-linux-x86_64.tar.gz
-mkdir -p $HOME/.local/opt
 mv google-cloud-sdk $HOME/.local/opt
 rm -f google-cloud-sdk-362.0.0-linux-x86_64.tar.gz
 popd
 pushd $HOME/.local/opt/google-cloud-sdk
-/usr/bin/env bash -c './install.sh --command-completion --path-update --quiet'
+/usr/bin/env bash -c './install.sh --command-completion true --path-update true --quiet'
 popd
