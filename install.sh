8db6135c7d8a21033dbf04936cd3224182295cf9
Carlos Pereira <carlos.l.pereira@gmail.com>
2021-11-26 16:51:57 +0100

    Install Terraform cli

--- install.sh
+++ install.sh
@@ -15,3 +15,10 @@ popd
 pushd $HOME/.local/opt/google-cloud-sdk
 /usr/bin/env bash -c './install.sh --command-completion true --path-update true --quiet'
 popd
+
+# Install Terraform
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
