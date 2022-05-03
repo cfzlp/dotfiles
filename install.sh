210b066a89db5cbea3551af0c62cfd4a9c3c1b08
Carlos Pereira <carlos.l.pereira@gmail.com>
2022-05-03 11:17:44 +0200

    Install pyenv

--- install.sh
+++ install.sh
@@ -17,8 +17,10 @@ pushd $HOME/.local/opt/google-cloud-sdk
 popd
 
 # Install Terraform
-
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
+
+# Install pyenv
+curl https://pyenv.run | bash
