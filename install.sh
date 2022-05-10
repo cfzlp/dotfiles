cf212c19321660bcfcd17c18ed7549e0a923f629
Carlos Pereira <carlos.l.pereira@gmail.com>
2022-05-10 17:57:43 +0200

    Execute brew commands in new shell

--- install.sh
+++ install.sh
@@ -30,8 +30,6 @@ sudo apt update -y
 sudo apt-get install -y build-essential procps curl file git gcc
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/codespace/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install kubeseal
-brew update
-brew install kubeseal
+/bin/bash -c "brew update && brew install kubeseal"
