6f44ddde8fd7f9455680e99495ef44db52d27fd8
Carlos Pereira <carlos.l.pereira@gmail.com>
2022-05-10 15:13:00 +0200

    Install homebrew for Linux

--- install.sh
+++ install.sh
@@ -24,3 +24,7 @@ sudo apt-get update && sudo apt-get install terraform
 
 # Install pyenv
 curl https://pyenv.run | bash
+
+# Install homebrew
+sudo apt-get install build-essential procps curl file git
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
