0d9626122d18f17502b073e2496fb59886bfb3a9
Carlos Pereira <carlos.l.pereira@gmail.com>
2022-05-18 12:04:18 +0200

    Update Google Cloud SDK

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ download_dir=$default_download_dir
 # Install Google Cloud SDK
 pushd $download_dir
 mkdir -p $HOME/.local/opt
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-362.0.0-linux-x86_64.tar.gz
-tar xzf google-cloud-sdk-362.0.0-linux-x86_64.tar.gz
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-386.0.0-linux-x86_64.tar.gz
+tar xzf google-cloud-cli-386.0.0-linux-x86_64.tar.gz
 mv google-cloud-sdk $HOME/.local/opt
-rm -f google-cloud-sdk-362.0.0-linux-x86_64.tar.gz
+rm -f google-cloud-cli-386.0.0-linux-x86_64.tar.gz
 popd
 pushd $HOME/.local/opt/google-cloud-sdk
 /usr/bin/env bash -c './install.sh --command-completion true --path-update true --quiet'
