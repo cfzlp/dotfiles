c252a7adf7ce503d2fa78cdbe51d9bbfd229c7ee
Carlos Pereira <carlos.l.pereira@gmail.com>
2022-10-21 10:31:04 +0200

    Change pyenv installation

--- install.sh
+++ install.sh
@@ -17,7 +17,13 @@ pushd $HOME/.local/opt/google-cloud-sdk
 popd
 
 # Install pyenv
-curl https://pyenv.run | bash
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
 
 # Install homebrew
 sudo apt-get update -y && sudo apt-get install -y build-essential procps curl file git gcc
